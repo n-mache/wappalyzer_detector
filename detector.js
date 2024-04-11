97eb4a6f2cd14de5ebef3026fbe9b3b2932a452d
まちぇ <n.mache.work@gmail.com>
2024-04-11 21:52:48 +0900

    Update detector.js

--- detector.js
+++ detector.js
@@ -1,20 +1,10 @@
-window.addEventListener('load', function(){
-    var wapl_detected = false;
-    var observer = new MutationObserver(function (ms){
-        console.log(ms);
-        ms.forEach(m=>{
-            if (m.addedNodes.length === 0){return;}
-            m.addedNodes.forEach(t=>{
-                if (t.tagName === "SCRIPT"){
-                    if (t.src.startsWith("chrome-extension://") && (t.src.endsWith("/js/js.js") || t.src.endsWith("/js/dom.js"))){
-                        if (!wapl_detected && window["on_wapl_detected"]!==undefined){
-                            window["on_wapl_detected"]();
-                        }
-                        wapl_detected = true;
-                    }
-                }
-            });
-        });
-    });
-    observer.observe(document.body, {childList: true});
-});
+function check_wappalyzer() {
+    try{
+        var ajax = new XMLHttpRequest();
+        ajax.open("GET", "chrome-extension://gppongmhjkpfnbhagpmjfkannfbllamg/js/js.js",false);
+        ajax.send();
+        return true;
+    }catch(e){
+        return false;
+    }
+}
